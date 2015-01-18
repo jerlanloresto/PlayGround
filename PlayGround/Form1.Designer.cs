5f0674761ec1afb4cf1f74680e0cacebef466d5a
gibbs08 <gilbertbejasa@gmail.com>
2015-01-18 14:50:08 +0800

    Add qrcode

--- PlayGround/Form1.Designer.cs
+++ PlayGround/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.c1QRCode1 = new C1.Win.C1BarCode.C1QRCode();
             this.SuspendLayout();
             // 
             // textBox1
@@ -48,11 +49,20 @@
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // c1QRCode1
+            // 
+            this.c1QRCode1.Location = new System.Drawing.Point(310, 216);
+            this.c1QRCode1.Name = "c1QRCode1";
+            this.c1QRCode1.Size = new System.Drawing.Size(75, 64);
+            this.c1QRCode1.TabIndex = 2;
+            this.c1QRCode1.Text = "c1QRCode1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1003, 387);
+            this.Controls.Add(this.c1QRCode1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
@@ -66,6 +76,7 @@
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
+        private C1.Win.C1BarCode.C1QRCode c1QRCode1;
     }
 }
 
