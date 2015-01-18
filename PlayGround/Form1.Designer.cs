9d08c637d1e3e0ba4fe997e8eb57264722ea2bf5
SharkSpeed <ijsantiagoiii@gmail.com>
2015-01-18 15:11:41 +0800

    Added Combobox

--- PlayGround/Form1.Designer.cs
+++ PlayGround/Form1.Designer.cs
@@ -28,11 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.c1FlexGrid1 = new C1.Win.C1FlexGrid.C1FlexGrid();
+            this.c1Combo1 = new C1.Win.C1List.C1Combo();
             ((System.ComponentModel.ISupportInitialize)(this.c1FlexGrid1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1Combo1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
@@ -70,11 +73,35 @@
             this.c1FlexGrid1.Size = new System.Drawing.Size(240, 150);
             this.c1FlexGrid1.TabIndex = 3;
             // 
+            // c1Combo1
+            // 
+            this.c1Combo1.AddItemSeparator = ';';
+            this.c1Combo1.Caption = "";
+            this.c1Combo1.CharacterCasing = System.Windows.Forms.CharacterCasing.Normal;
+            this.c1Combo1.DeadAreaBackColor = System.Drawing.Color.Empty;
+            this.c1Combo1.EditorBackColor = System.Drawing.SystemColors.Window;
+            this.c1Combo1.EditorFont = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c1Combo1.EditorForeColor = System.Drawing.SystemColors.WindowText;
+            this.c1Combo1.Images.Add(((System.Drawing.Image)(resources.GetObject("c1Combo1.Images"))));
+            this.c1Combo1.Location = new System.Drawing.Point(769, 42);
+            this.c1Combo1.MatchEntryTimeout = ((long)(2000));
+            this.c1Combo1.MaxDropDownItems = ((short)(5));
+            this.c1Combo1.MaxLength = 32767;
+            this.c1Combo1.MouseCursor = System.Windows.Forms.Cursors.Default;
+            this.c1Combo1.Name = "c1Combo1";
+            this.c1Combo1.RowDivider.Style = C1.Win.C1List.LineStyleEnum.None;
+            this.c1Combo1.RowSubDividerColor = System.Drawing.Color.DarkGray;
+            this.c1Combo1.Size = new System.Drawing.Size(121, 21);
+            this.c1Combo1.TabIndex = 4;
+            this.c1Combo1.Text = "c1Combo1";
+            this.c1Combo1.PropBag = resources.GetString("c1Combo1.PropBag");
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1003, 394);
+            this.Controls.Add(this.c1Combo1);
             this.Controls.Add(this.c1FlexGrid1);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button1);
@@ -82,6 +109,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.c1FlexGrid1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1Combo1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -93,6 +121,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.CheckBox checkBox1;
         private C1.Win.C1FlexGrid.C1FlexGrid c1FlexGrid1;
+        private C1.Win.C1List.C1Combo c1Combo1;
     }
 }
 
